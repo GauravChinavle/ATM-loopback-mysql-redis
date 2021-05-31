30798f592814fe2af6c1fa3c05b7e8092166c350
GauravChinavle <chinawalegaurav@gmail.com>
2021-06-01 01:31:09 +0530

    credit/debit takes input amount & session_id

--- common/models/auth.js
+++ common/models/auth.js
@@ -1,50 +1,53 @@
-const redis = require('redis');
-var _ = require('lodash');
+const redis = require("redis");
+var _ = require("lodash");
 const REDIS_PORT = process.env.PORT || 6379;
 const client = redis.createClient(REDIS_PORT);
-var loopback = require('loopback');
+var loopback = require("loopback");
 let Customer = loopback.getModel("customer");
 
-module.exports.loginSession = async function (data){
-    let count = await new Promise((resolve, reject) => {
-        Customer.count(data, (err, count) => {
-          if (err) reject(err) // Reject here
-          else resolve(count);
-        });
-      });
-      if(!count){
-          return "User not available"
-      }
+module.exports.loginSession = async function(data) {
+  let count = await new Promise((resolve, reject) => {
+    Customer.count(data, (err, count) => {
+      if (err) reject(err);
+      // Reject here
+      else resolve(count);
+    });
+  });
+  if (!count) {
+    return "User not available";
+  }
 
-      data.session_id = ""+Math.floor(Math.random() * 1000);
-      client.setex( data.session_id, 50000000, JSON.stringify(data));
-      console.log("Logging in with account no : "+data.account_no);
-      return "Session id is "+data.session_id
+  data.session_id = "" + Math.floor(Math.random() * 1000);
+  client.setex(data.session_id, 50000000, JSON.stringify(data));
+  console.log("Logging in with account no : " + data.account_no);
+  return "Session id is " + data.session_id;
 };
 
-
-module.exports.isLoggedIn = async function ({session_id}){
-    let result = await new Promise((resolve, reject) => {
-        client.get(session_id, (err, data) => {
-          if (err) reject(err) // Reject here
-          else resolve(data);
-        });
-      });
-      console.log("isLoggedIn : "+!_.isEmpty(result));
-      if(_.isEmpty(result)){
-        return false;
-      }
-      return true;
+module.exports.isLoggedIn = async function({ session_id }) {
+  let result = await new Promise((resolve, reject) => {
+    client.get(session_id, (err, data) => {
+      if (err) reject(err);
+      // Reject here
+      else resolve(data);
+    });
+  });
+  console.log("isLoggedIn : " + !_.isEmpty(result));
+  if (_.isEmpty(result)) {
+    return false;
+  }
+  console.log(JSON.parse(result));
+  return JSON.parse(result);
 };
 
-module.exports.logoutSession = async function ({session_id}){
-    let result = await new Promise((resolve, reject) => {
-        client.del( ""+session_id, (err, data) => {
-            if (err) reject(err) // Reject here
-            else resolve(data);
-          });
-      });
-      if(result === 0) return "Session was not present."
-      console.log("Logging out with session id :" + session_id);
-      return "You are logged out"
+module.exports.logoutSession = async function({ session_id }) {
+  let result = await new Promise((resolve, reject) => {
+    client.del("" + session_id, (err, data) => {
+      if (err) reject(err);
+      // Reject here
+      else resolve(data);
+    });
+  });
+  if (result === 0) return "Session was not present.";
+  console.log("Logging out with session id :" + session_id);
+  return "You are logged out";
 };
--- common/models/transaction.js
+++ common/models/transaction.js
@@ -1,36 +1,42 @@
 "use strict";
-const auth = require('./auth');
+const auth = require("./auth");
 
 module.exports = function(Transaction) {
   Transaction.debit = async data => {
-    if(await auth.isLoggedIn(data) === false){
-      return "Session expired"
+    const response = await auth.isLoggedIn(data);
+    if (response === false) {
+      return "Session expired";
     }
-    let {amount} = await new Promise((resolve, reject) => {
+    data.account_no = response.account_no;
+    let { amount } = await new Promise((resolve, reject) => {
       data.transaction_id = "DB" + Math.floor(Math.random() * 100000);
       data.transaction_type = "DEBIT";
       Transaction.create(data, (err, results) => {
-        if (err) reject(err) // Reject here
+        if (err) reject(err);
+        // Reject here
         else resolve(results);
       });
     });
-    console.log(amount + " debited to your account.");
-    return amount + " debited from your account."
+    console.log(amount + " debited from Account number : " + data.account_no);
+    return amount + " debited from Account number : " + data.account_no;
   };
 
   Transaction.credit = async data => {
-    if(await auth.isLoggedIn(data) === false){
-      return "Session timeout"
+    const response = await auth.isLoggedIn(data);
+    if (response === false) {
+      return "Session expired";
     }
-    let {amount} = await new Promise((resolve, reject) => {
+    data.account_no = response.account_no;
+    let { amount } = await new Promise((resolve, reject) => {
       data.transaction_id = "CR" + Math.floor(Math.random() * 100000);
       data.transaction_type = "CREDIT";
       Transaction.create(data, (err, results) => {
-        if (err) reject(err); // Reject here
+        if (err) reject(err);
+        // Reject here
         else resolve(results);
       });
     });
-    console.log(amount + " credited to your account.");
-    return amount + " credited to your account."
+    console.log(amount + " credited to Account number : " + data.account_no);
+    return amount + " credited to Account number : " + data.account_no;
   };
 };
