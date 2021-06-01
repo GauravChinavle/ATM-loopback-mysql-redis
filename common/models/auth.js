97db03f3e213819c508fc1e9e53fc757f84a7a9a
GauravChinavle <chinawalegaurav@gmail.com>
2021-06-01 12:05:30 +0530

    Adds .env

--- common/models/auth.js
+++ common/models/auth.js
@@ -8,16 +8,14 @@ let Customer = loopback.getModel("customer");
 module.exports.loginSession = async function(data) {
   let count = await new Promise((resolve, reject) => {
     Customer.count(data, (err, count) => {
-      if (err) reject(err);
-      // Reject here
-      else resolve(count);
+      err ? reject(err): resolve(count)
     });
   });
   if (!count) {
     return "User not available";
   }
 
-  data.session_id = "" + Math.floor(Math.random() * 1000);
+  data.session_id = Math.floor(Math.random() * 1000);
   client.setex(data.session_id, 50000000, JSON.stringify(data));
   console.log("Logging in with account no : " + data.account_no);
   return "Session id is " + data.session_id;
@@ -26,25 +24,20 @@ module.exports.loginSession = async function(data) {
 module.exports.isLoggedIn = async function({ session_id }) {
   let result = await new Promise((resolve, reject) => {
     client.get(session_id, (err, data) => {
-      if (err) reject(err);
-      // Reject here
-      else resolve(data);
+       err ? reject(err) : resolve(data);
     });
   });
   console.log("isLoggedIn : " + !_.isEmpty(result));
   if (_.isEmpty(result)) {
     return false;
   }
-  console.log(JSON.parse(result));
   return JSON.parse(result);
 };
 
 module.exports.logoutSession = async function({ session_id }) {
   let result = await new Promise((resolve, reject) => {
-    client.del("" + session_id, (err, data) => {
-      if (err) reject(err);
-      // Reject here
-      else resolve(data);
+    client.del(session_id, (err, data) => {
+      err ? reject(err) : resolve(data)
     });
   });
   if (result === 0) return "Session was not present.";
--- common/models/transaction.js
+++ common/models/transaction.js
@@ -1,4 +1,5 @@
 "use strict";
+require('dotenv').config()
 const auth = require("./auth");
 
 module.exports = function(Transaction) {
@@ -9,12 +10,10 @@ module.exports = function(Transaction) {
     }
     data.account_no = response.account_no;
     let { amount } = await new Promise((resolve, reject) => {
-      data.transaction_id = "DB" + Math.floor(Math.random() * 100000);
-      data.transaction_type = "DEBIT";
+      data.transaction_id = process.env.TYPE_DEBIT + Math.floor(Math.random() * 100000);
+      data.transaction_type = process.env.TYPE_DEBIT;
       Transaction.create(data, (err, results) => {
-        if (err) reject(err);
-        // Reject here
-        else resolve(results);
+        err ? reject(err) : resolve(results)
       });
     });
     console.log(amount + " debited from Account number : " + data.account_no);
@@ -28,12 +27,10 @@ module.exports = function(Transaction) {
     }
     data.account_no = response.account_no;
     let { amount } = await new Promise((resolve, reject) => {
-      data.transaction_id = "CR" + Math.floor(Math.random() * 100000);
-      data.transaction_type = "CREDIT";
+      data.transaction_id = process.env.TYPE_CREDIT + Math.floor(Math.random() * 100000);
+      data.transaction_type = process.env.TYPE_CREDIT;
       Transaction.create(data, (err, results) => {
-        if (err) reject(err);
-        // Reject here
-        else resolve(results);
+        err ? reject(err) : resolve(results)
       });
     });
     console.log(amount + " credited to Account number : " + data.account_no);
